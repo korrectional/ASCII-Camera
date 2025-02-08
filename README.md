f8b68a029dec2630d5af22c262eca7379afd663c
Korrectional <156260369+korrectional@users.noreply.github.com>
2025-02-07 21:52:31 -0500

    Ability to change resolution

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # ASCII-Camera
-This python script taken in the information from the camera (using ```cv2```) and transforms it into ASCII art, realtime.
-### To run: ```python main.py```
-### libraries: ```cv2, os```
-<img src="Pic.png" alt="Me" width="300">
+
+See camera output in the terminal!
+
+This Python script takes in information from the camera (using `cv2`) and transforms it into ASCII art, in real-time.
+
+### To run: `python main.py [desired resolution level]`
+
+<img src="docs/Pic.png" alt="Me" width="300">
+
+_Level 1 resolution_
+
+### Resolution level
+
+Default is 1, but if you want an experience with lower resolution you can do higher values
+
+<img src="docs/lowResPic.png" alt="Me" width="300">
+
+_Level 4 resolution_
+
+##### How it works: instead of analysing each pixel and making it an ascii character, the program will only go over certain pixels (ex: if 2 then only translate every 2 pixels)
--- main.py
+++ main.py
@@ -1,48 +1,54 @@
 import cv2
 import os
+import argparse
+
+# arguments
+parser = argparse.ArgumentParser("ASCII camera")
+parser.add_argument(
+    "resolution", help="The higher the number, the lower the resolution (1 for default)", type=int)
+args = parser.parse_args()
 
 
 # 92
-characters = " `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi}{CfI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
+characters = "         `.-':_,^=;><+!rc*/z?Bg0MNWQ%&@"
+
 
 def key(val):
-    val = (int)((val/256) * 92)
+    val = (int)((val/256) * len(characters))
+
+    return " " + characters[val] + " "
 
 
-    
-
-    return characters[val]
 key(2)
 
 print("Loading ...")
 
-#cv2.namedWindow("preview")
+# cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 vc.set(3, 176)
 vc.set(4, 176)
 x = 176
 y = 144
 
-if(vc.isOpened()):
+if (vc.isOpened()):
     rval, frame = vc.read()
 else:
     rval = False
 
-while(rval):
+while (rval):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #cv2.imshow("preview", gray)
+    # cv2.imshow("preview", gray)
 
     image = ""
-    for i in range(0, y-1):
+    for i in range(0, y-1, args.resolution):
         image += "\n"
-        for j in range(0, x):
+        for j in range(0, x, args.resolution):
             image += key(gray[i][j])
-    
+
     os.system("cls")
     image += "\n"
     print(image)
 
-    
     rval, frame = vc.read()
     key_press = cv2.waitKey(20)
     if key_press == 27:
